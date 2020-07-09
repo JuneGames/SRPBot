ead5fa10755e161747cf72e69a56fd8c429e9ae3
JuneGames <68033326+JuneGames@users.noreply.github.com>
2020-07-09 14:05:40 +0200

    init
    init

--- index.js
+++ index.js
@@ -113,6 +113,6 @@ bot.on("message", async message => {
 })
 
 bot.login(process.env.token);
-app.listen(3000, function () {
+bot.listen(3000, function () {
     console.log('SRPBot app listening on port 3000!');
   })
